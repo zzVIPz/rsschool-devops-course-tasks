15207e2d61814bd2181b7983b488ebf6b28da346
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-10-01 23:10:28 -0400

    add docs, orginize code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "s3_bucket_tfstates" {
-  bucket = "zzvip-devops-tfstate-20240928"
+  bucket = var.bucket_name
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  default = "zzvip-devops-tfstate-20240928"
+}

df7406b20fc4399d1d9808c3f5efbf08a3631468
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-10-01 22:50:21 -0400

    add gitignore

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "s3bucket" {
-  bucket = "zzvip-devops-tfstate"
+resource "aws_s3_bucket" "s3_bucket_tfstates" {
+  bucket = "zzvip-devops-tfstate-20240928"
 }
